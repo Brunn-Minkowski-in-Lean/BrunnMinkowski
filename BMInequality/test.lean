53f2d70c7bce13fab4b45fd418c6adfb7aad7823
Hyojae Lim <hyojaelim@Hyojaeui-MacBookAir.local>
2025-01-14 13:46:26 +0100

    finding Mathlib lemmas

---
+++ BMInequality/test.lean
@@ -0,0 +1,42 @@
+import Mathlib
+-- #min_imports
+
+-- LeanSearch
+import LeanSearchClient
+
+#leansearch "Union of two compact sets is also compact?"
+--
+
+-- InnerRegularity
+
+-- InnerRegular mu : Prop
+-- Need: InnerRegular volume
+
+#check MeasurableSet.exists_isCompact_diff_lt
+-- Want to apply the lemma above to [0, 1] in R
+#check Set.Ioo 0 1
+#check MeasurableSet.exists_isCompact_diff_lt (measurableSet_Ioo : MeasurableSet (Set.Ioo 0 1))
+
+variable (A B : Set ℝ) (hA : MeasurableSet A) (hB : MeasurableSet B)
+#check MeasurableSet.exists_isCompact_diff_lt hA
+
+--
+
+-- translation invariance of measure --
+
+open Set Filter MeasureTheory MeasureTheory.Measure TopologicalSpace
+
+theorem measure_translation_of_set (s : Set ℝ) (c : ℝ) : volume (image (fun x ↦ x + c) s) = volume s := by
+  simp only [image_add_right, measure_preimage_add_right]
+
+#check measure_translation_of_set
+
+--
+
+#check measure_union_add_inter
+
+#check MeasureTheory.measure_mono
+
+
+#check IsCompact.sSup_mem
+#check IsCompact.sInf_mem
