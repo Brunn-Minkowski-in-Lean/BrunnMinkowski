caee00860574e1a7e36c6fa892698e24055a76b8
Jineon Baek <jineon@umich.edu>
2025-01-16 11:39:43 +0900

    induction on dimension attempt

--- BMInequality/PrekopaLeindler.lean
+++ BMInequality/PrekopaLeindler.lean
@@ -2,11 +2,26 @@ import Mathlib.Analysis.InnerProductSpace.Basic
 import Mathlib.Analysis.InnerProductSpace.PiL2
 import Mathlib.MeasureTheory.Integral.Lebesgue
 import Mathlib.Analysis.SpecialFunctions.Gamma.Basic
+import Mathlib.Analysis.InnerProductSpace.EuclideanDist
 
 open NNReal ENNReal MeasureTheory Finset
 open Real Set MeasureTheory Filter Asymptotics
 
 open scoped Real Topology
+
+-- isomorhpism from any f.d. R-v.s. to R^d
+#check toEuclidean
+
+theorem EuclideanSpace.induction_on_dimension
+    {P : (α : Type) →
+      [AddCommGroup α] → [TopologicalSpace α] →  [TopologicalAddGroup α] → [T2Space α] → [Module ℝ α] → [ContinuousSMul ℝ α] → [FiniteDimensional ℝ α] → Sort}
+    {base0 : P (EuclideanSpace ℝ (Fin 0))}
+    {base1 : P ℝ}
+    {induct : {α β : Type} →
+      [AddCommGroup α] → [TopologicalSpace α] →  [TopologicalAddGroup α] → [T2Space α] → [Module ℝ α] → [ContinuousSMul ℝ α] → [FiniteDimensional ℝ α] →
+      [AddCommGroup β] → [TopologicalSpace β] →  [TopologicalAddGroup β] → [T2Space β] → [Module ℝ β] → [ContinuousSMul ℝ β] → [FiniteDimensional ℝ β] →
+      P α → P β → P (α × β)} :
+  (α : Type) → [AddCommGroup α] → [TopologicalSpace α] →  [TopologicalAddGroup α] → [T2Space α] → [Module ℝ α] → [ContinuousSMul ℝ α] → [FiniteDimensional ℝ α] → P α := by sorry
 
 theorem ineqPrekopaLeindler
     {t : ℝ} (h0t : 0 < t) (ht1 : t < 1)
