ed16d3b805b350a78e912127be02bd81d9e4c0d9
Jineon Baek <jineon@umich.edu>
2025-01-16 10:54:37 +0900

    state PL ineq

--- BMInequality/PrekopaLeindler.lean
+++ BMInequality/PrekopaLeindler.lean
@@ -1,5 +1,19 @@
 import Mathlib.Analysis.InnerProductSpace.Basic
 import Mathlib.Analysis.InnerProductSpace.PiL2
+import Mathlib.MeasureTheory.Integral.Lebesgue
+import Mathlib.Analysis.SpecialFunctions.Gamma.Basic
+
+open NNReal ENNReal MeasureTheory Finset
+open Real Set MeasureTheory Filter Asymptotics
+
+open scoped Real Topology
 
 theorem ineqPrekopaLeindler
-  {t : ℝ} (h0t : 0 < t) (ht1 : t < 1) : 1 = 1 := by sorry
+    {t : ℝ} (h0t : 0 < t) (ht1 : t < 1)
+    {d : ℕ} (f g h : EuclideanSpace ℝ (Fin d) → ℝ)
+    (hlb :
+      ∀ x y : EuclideanSpace ℝ (Fin d),
+      (f x)^(1 - t) * (g y)^t ≤ h (x + y)) :
+  (∫ x, f x)^(1-t) * (∫ y, g y)^t ≤
+  (1 - t)^(d * (1-t)) * t^(d*t) * (∫ x, h x)
+  := by sorry
