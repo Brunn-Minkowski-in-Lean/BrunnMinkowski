069e3d59efd35373caf922ace6901b2187541159
qawbecrdtey <qawbecrdtey@kaist.ac.kr>
2025-01-21 01:28:21 +0900

    refactoring.

--- BMInequality/Curve.lean
+++ BMInequality/Curve.lean
@@ -3,76 +3,42 @@ import Mathlib
 structure Curve' (X : Type*) [TopologicalSpace X] where
   func : ℝ → X
   interval : Set ℝ
-  interval_is_connected : IsConnected interval
-  interval_is_compact : IsCompact interval
-  interval_is_nonempty : interval.Nonempty
-  func_is_continuous : ContinuousOn func (Set.Icc 0 1)
+  isCompact_interval : IsCompact interval
+  isConnected_interval : IsConnected interval
+  nonempty_interval : interval.Nonempty
+  func_continuousOn : ContinuousOn func interval
 
 namespace Curve'
 
 variable {X : Type*} [TopologicalSpace X]
+variable {c : Curve' X}
 
-class Closed (c : Curve' X) : Prop where
-  isClosed : c.func (sInf c.interval) = c.func (sSup c.interval)
+protected noncomputable def lo (c : Curve' X) := sInf c.interval
 
-class Simple (c : Curve' X) : Prop where
-  isSimple : Set.InjOn c.func c.interval
+protected noncomputable def hi (c : Curve' X) := sSup c.interval
 
-class Arc (c : Curve' X) : Prop where
-  isArc : c.func 0 ≠ c.func 1
+@[simp]
+theorem icc_lo_hi_eq_interval : Set.Icc c.lo c.hi = c.interval :=
+  (eq_Icc_of_connected_compact c.isConnected_interval c.isCompact_interval).symm
 
-class SimpleClosed (c : Curve' X) extends Closed c : Prop where
-  isSimple : Set.InjOn c.func (Set.Ico 0 1) ∧ c.func 0 = c.func 1
+protected def mk'
+    (f : ℝ → X) (a b : ℝ) (h₁ : a ≤ b) (h₂ : ContinuousOn f (Set.Icc a b)) : Curve' X :=
+  ⟨f, Set.Icc a b, isCompact_Icc, isConnected_Icc h₁, Set.nonempty_Icc.mpr h₁, h₂⟩
 
-class SimpleArc (c : Curve' X) extends Arc c : Prop where
-  isSimple : Set.InjOn c.func (Set.Icc 0 1)
+def IsClosed (c : Curve' X) : Prop :=
+  c.func (sInf c.interval) = c.func (sSup c.interval)
 
-def ProperlyEquiv (c₁ c₂ : Curve' X) : Prop :=
-  ∃ f : ℝ → ℝ,
-    StrictMonoOn f c₂.interval ∧
-    ContinuousOn f c₂.interval ∧
-    c₁.func (f (sInf c₁.interval)) = c₂.func (sInf c₂.interval) ∧
-    c₁.func (f (sSup c₁.interval)) = c₂.func (sSup c₂.interval) ∧
-    ∀ t ∈ c₂.interval, c₂.func t = c₁.func (f t)
+def IsSimple (c : Curve' X) : Prop :=
+  Set.InjOn c.func c.interval
 
-def ImproperlyEquiv (c₁ c₂ : Curve' X) : Prop :=
-  ∃ f : ℝ → ℝ,
-    StrictAntiOn f c₂.interval ∧
-    ContinuousOn f c₂.interval ∧
-    c₁.func (f (sSup c₁.interval)) = c₂.func (sInf c₂.interval) ∧
-    c₁.func (f (sInf c₁.interval)) = c₂.func (sSup c₂.interval) ∧
-    ∀ t ∈ c₂.interval, c₂.func t = c₁.func (f t)
+def IsArc (c : Curve' X) : Prop :=
+  c.func c.lo ≠ c.func c.hi
 
-instance isSetoid : Setoid (Curve' X) where
-  r c₁ c₂ := ProperlyEquiv c₁ c₂ ∨ ImproperlyEquiv c₁ c₂
-  iseqv :=
-   ⟨fun _ ↦ .inl ⟨id, strictMonoOn_id, continuousOn_id, rfl, rfl, fun _ _ ↦ rfl⟩,
-    fun h ↦ by
-      rcases h with h | h
-      · sorry
-      · sorry,
-    fun h₁ h₂ ↦ by
-      rcases h₁ with h₁ | h₁ <;> rcases h₂ with h₂ | h₂
-      · sorry
-      · sorry
-      · sorry
-      · sorry⟩
+def IsSimpleClosed (c : Curve' X) : Prop :=
+  IsClosed c ∧ Set.InjOn c.func c.interval ∧ c.func c.lo = c.func c.hi
 
-def rel_def (c₁ c₂ : Curve' X) : c₁ ≈ c₂ ↔ ProperlyEquiv c₁ c₂ ∨ ImproperlyEquiv c₁ c₂ :=
-  ⟨id, id⟩
+def IsSimpleArc (c : Curve' X) : Prop :=
+  IsArc c ∧ Set.InjOn c.func c.interval
 
 end Curve'
-
-def Curve (X : Type*) [TopologicalSpace X] :=
-  Quotient (@Curve'.isSetoid X _)
-
-namespace Curve
-
-variable {X : Type*} [TopologicalSpace X]
-variable {c c₁ c₂ c₃ : Curve X}
-
-instance : Coe (Curve' X) (Curve X) :=
-  ⟨Quot.mk _⟩
-
-end Curve
 
