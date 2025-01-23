85869f4689ee1e5d686f478666ad49f7b90c729d
qawbecrdtey <qawbecrdtey@kaist.ac.kr>
2025-01-23 21:59:14 +0900

    Trying to make sense.

--- JordanCurveTheorem/JordanCurveTheorem.lean
+++ JordanCurveTheorem/JordanCurveTheorem.lean
@@ -4,36 +4,77 @@ import Mathlib
 
 local notation "ℝ²" => EuclideanSpace ℝ (Fin 2)
 
-section
-
-variable {p : ENNReal} {α : ι → Type*} [Fact (1 ≤ p)]
-
---open Set EMetric in
---instance : TopologicalSpace (PiLp p α) where
---  IsOpen s :=
---    ∃ S : Set (Set (PiLp p α)), (∀ t ∈ S, ∃ (x : PiLp p α) (ε : ENNReal), t = ball x ε) ∧ s = ⋃₀ S
---  isOpen_univ := ⟨{s : Set (PiLp p α) | ∃ x, s = ball x 1}, by
---    simp only [mem_setOf_eq, forall_exists_index, forall_eq_apply_imp_iff, exists_apply_eq_apply2',
---      implies_true, true_and]
---    ext x; simp_rw [mem_sUnion, mem_setOf_eq]
---    exact
---      ⟨fun _ ↦ ⟨ball x 1, ⟨⟨x, rfl⟩, mem_ball.mpr (edist_self x ▸ zero_lt_one)⟩⟩,
---        fun _ ↦ mem_univ _⟩⟩
-
-end
-
-variable {x y : ℝ²}
-
-/-- A Jordan curve is an injective path. -/
-structure JordanCurve (x y : ℝ²) extends Path x y where
+/-- A Jordan arc is an injective path. -/
+structure JordanArc (s t : ℝ²) extends Path s t where
   injective_toFun : Function.Injective toFun
 
-def jordanCurve_separated_components (c : JordanCurve x x) :=
-  TopologicalSpace.induced (fun x : {a : ℝ² // ∀ x, a ≠ c.toFun x} ↦ x.val)
-    (TopologicalSpace.generateFrom (x ∈ ·))
+namespace JordanArc
 
-theorem jordan_curve_theorem
-    {c : JordanCurve x x} :
-    (@Set.univ (ConnectedComponents {a : ℝ² // ∀ x, a ≠ c.toPath x})).ncard = 2 := by
+variable {s t : ℝ²}
+variable {a : JordanArc s t}
+
+instance : FunLike (JordanArc s t) unitInterval ℝ² where
+  coe a := a.toFun
+  coe_injective' := sorry
+
+theorem jordanPath_source_ne_target (a : JordanArc s t) : s ≠ t := by
+  intro h; simp_rw [← a.source', ← a.target'] at h; exact zero_ne_one (a.injective_toFun h)
+
+end JordanArc
+
+/-- A Jordan curve is an continuous injective map from $S^1$. -/
+structure JordanCurve (p : ℝ²) extends Path p p where
+  almost_injective_toFun : ∀ {x y}, toFun x = toFun y → x = y ∨ (x = 0 ∧ y = 1) ∨ (x = 1 ∧ y = 0)
+
+namespace JordanCurve
+
+variable {p : ℝ²}
+variable {c : JordanCurve p}
+
+instance : FunLike (JordanCurve p) Circle ℝ² where
+  coe a x := sorry
+  coe_injective' := sorry
+
+open Set Metric
+
+theorem jordanCurve_bounded :
+    ∃ r, c.toFun '' univ ⊆ ball 0 r := by
   sorry
+
+-- TODO: Remove `noncomputable` if possible.
+/-- `outerComponent` is the outer component. -/
+noncomputable def outerComponent (c : JordanCurve p) : Set ℝ² :=
+  let r : ℝ := Classical.choose (@jordanCurve_bounded p c)
+  connectedComponentIn {q : ℝ² | ∀ x, q ≠ c.toFun x} !₂[r, 0]
+
+-- TODO: Remove `noncomputable` if possible.
+/-- `innerComponent` is the inner component. -/
+noncomputable def innerComponent (c : JordanCurve p) : Set ℝ² :=
+  {q : ℝ² | q ∉ outerComponent c ∧ ∀ x, q ≠ c x}
+
+theorem isOpen_outerComponent : IsOpen c.outerComponent :=
+  sorry
+
+theorem isConnected_outerComponent :
+    IsConnected c.outerComponent :=
+  isConnected_connectedComponentIn_iff.mpr (by
+    simp only [ContinuousMap.toFun_eq_coe, Path.coe_toContinuousMap, ne_eq, Subtype.forall,
+      mem_Icc, image_univ, mem_setOf_eq]
+    rintro r ⟨h₁, h₂⟩ h₃
+
+    sorry)
+
+theorem isOpen_innerComponent : IsOpen c.innerComponent :=
+  sorry
+
+-- Jordan Curve theorem
+theorem isConnected_innerComponent :
+    IsConnected c.innerComponent :=
+  sorry
+
+end JordanCurve
+
+-- TODO: Should we generalize this?
+-- 1. Jordan–Brouwer separation theorem
+-- 2. Jordan–Schoenflies theorem
 
