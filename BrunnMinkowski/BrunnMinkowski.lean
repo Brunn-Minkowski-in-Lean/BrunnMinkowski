723b9e1e636d40163388044657ffdc91b9f1f823
Seungheon Oh <seungheon.ooh@gmail.com>
2025-01-29 19:27:49 -0600

    Add requirement directly

--- BrunnMinkowski/BrunnMinkowski.lean
+++ BrunnMinkowski/BrunnMinkowski.lean
@@ -5,12 +5,12 @@ import BrunnMinkowski.EuclideanSpace
 
 open scoped Pointwise NNReal
 
-variable {I : Type} [Fintype I] {n : Set.Ici (1 : ℕ)}
+variable {I : Type} [Fintype I] {n : ℕ}
 
 noncomputable def ConvexBody.volume (A : ConvexBody (ℝI I)) : ENNReal :=
   MeasureTheory.volume (A : Set (ℝI I))
 
-def brunn_minkowski (A B : ConvexBody (ℝn n)) :
+def brunn_minkowski (A B : ConvexBody (ℝn n)) (ngz : n ≠ 0) :
     A.volume ^ (n⁻¹ : ℝ) + B.volume ^ (n⁻¹ : ℝ) ≤
     (A + B).volume ^ (n⁻¹ : ℝ) := by
   sorry
