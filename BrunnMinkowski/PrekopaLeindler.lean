010134a57900a752990cd26c1d2714804baac444
qawbecrdtey <qawbecrdtey@kaist.ac.kr>
2025-02-09 00:54:44 +0900

    unrefactor.

--- BrunnMinkowski/PrekopaLeindler.lean
+++ BrunnMinkowski/PrekopaLeindler.lean
@@ -5,6 +5,7 @@ import Mathlib.Analysis.SpecialFunctions.Gamma.Basic
 import Mathlib.Analysis.InnerProductSpace.EuclideanDist
 import Mathlib.Analysis.RCLike.Basic
 import BrunnMinkowski.EuclideanSpace
+import Mathlib
 
 open NNReal ENNReal MeasureTheory Finset
 open Real Set MeasureTheory Filter Asymptotics
@@ -14,52 +15,49 @@ open scoped Real Topology
 -- isomorhpism from any f.d. R-v.s. to R^d
 #check toEuclidean
 
-class EuclideanBaseStructure (α : Type*) extends
-  AddCommGroup α,
-  TopologicalSpace α,
-  T2Space α,
-  Module ℝ α,
-  ContinuousSMul ℝ α,
-  FiniteDimensional ℝ α
-
-instance {α : Type} [EuclideanBaseStructure α] {β : Type} [EuclideanBaseStructure β] :
-    EuclideanBaseStructure (α × β) :=
-  sorry
-
-instance {n : ℕ} : EuclideanBaseStructure (EuclideanSpace ℝ (Fin n)) :=
-  sorry
-
-class EuclideanIsoInvariant (P : (α : Type) → [EuclideanBaseStructure α] → Prop) : Prop where
+class EuclideanIsomorphismInvariant
+    (P : (α : Type) → [AddCommGroup α] → [TopologicalSpace α] → [T2Space α] → [Module ℝ α] →
+      [ContinuousSMul ℝ α] → [FiniteDimensional ℝ α] → Prop) :
+    Prop where
   isomorphism_invariant
-      (α : Type) [EuclideanBaseStructure α] (β : Type) [EuclideanBaseStructure β] (h : α ≃ₗ[ℝ] β) :
+      (α : Type) [AddCommGroup α] [TopologicalSpace α] [T2Space α] [Module ℝ α]
+        [ContinuousSMul ℝ α] [FiniteDimensional ℝ α]
+      (β : Type) [AddCommGroup β] [TopologicalSpace β] [T2Space β] [Module ℝ β]
+        [ContinuousSMul ℝ β] [FiniteDimensional ℝ β]
+      (h : α ≃ₗ[ℝ] β) :
     P α ↔ P β
 
-lemma asdf {n : ℕ} : Module.finrank ℝ (EuclideanSpace ℝ (Fin n)) = n :=
+example {n : ℕ} : Module.finrank ℝ (EuclideanSpace ℝ (Fin n)) = n :=
   finrank_euclideanSpace_fin
 
 -- TODO: Fix instance issues.
 -- TODO: Consider higher order types if possible.
 theorem EuclideanSpace.induction_on_finrank
-    {P : (α : Type) → [EuclideanBaseStructure α] → Prop} [EuclideanIsoInvariant P]
-    {base0 : (α : Type) → [EuclideanBaseStructure α] → Module.finrank ℝ α = 0 → P α}
-    {base1 : (α : Type) → [EuclideanBaseStructure α] → Module.finrank ℝ α = 1 → P α}
-    {induct : {α : Type} → [EuclideanBaseStructure α] → {β : Type} → [EuclideanBaseStructure β] →
+    {P : (α : Type) → [AddCommGroup α] → [TopologicalSpace α] → [T2Space α] → [Module ℝ α] →
+      [ContinuousSMul ℝ α] → [FiniteDimensional ℝ α] → Prop} [EuclideanIsomorphismInvariant P]
+    {base0 : (α : Type) → [AddCommGroup α] → [TopologicalSpace α] → [T2Space α] → [Module ℝ α] →
+      [ContinuousSMul ℝ α] → [FiniteDimensional ℝ α] → Module.finrank ℝ α = 0 → P α}
+    {base1 : (α : Type) → [AddCommGroup α] → [TopologicalSpace α] → [T2Space α] → [Module ℝ α] →
+      [ContinuousSMul ℝ α] → [FiniteDimensional ℝ α] → Module.finrank ℝ α = 1 → P α}
+    {induct : {α : Type} → [AddCommGroup α] → [TopologicalSpace α] → [T2Space α] → [Module ℝ α] →
+      [ContinuousSMul ℝ α] → [FiniteDimensional ℝ α] →
+      {β : Type} → [AddCommGroup β] → [TopologicalSpace β] → [T2Space β] → [Module ℝ β] →
+      [ContinuousSMul ℝ β] → [FiniteDimensional ℝ β] →
       P α → P β → P (α × β)}
-    (α : Type) [EuclideanBaseStructure α] : P α := by
+    (α : Type) [AddCommGroup α] [TopologicalSpace α] [T2Space α] [Module ℝ α]
+    [ContinuousSMul ℝ α] [FiniteDimensional ℝ α] : P α := by
   induction' h₀ : Module.finrank ℝ α using Nat.strong_induction_on generalizing α
-  case h n ih _ =>
+  case h n ih _ _ _ _ _ _ =>
     match n with
     | 0 => exact base0 _ h₀
     | 1 => exact base1 _ h₀
     | n + 2 =>
-      refine (‹EuclideanIsoInvariant P›.isomorphism_invariant α
-          ((EuclideanSpace ℝ (Fin (n + 1))) × (EuclideanSpace ℝ (Fin 1))) ?_).mpr
+      let eqv : α ≃ₗ[ℝ] (EuclideanSpace ℝ (Fin (n + 1)) × EuclideanSpace ℝ (Fin 1)) :=
+        sorry
+      exact (‹EuclideanIsomorphismInvariant P›.isomorphism_invariant α _ eqv).mpr
         (induct
-          (ih (n + 1) (by omega) (EuclideanSpace ℝ (Fin (n + 1))) ?_)
-          (ih 1 (by omega) _ ?_))
-      · sorry
-      · sorry -- exact @finrank_euclideanSpace_fin ℝ _ (n + 1)
-      · sorry -- exact finrank_euclideanSpace_fin
+          (ih (n + 1) (by omega) _ finrank_euclideanSpace_fin)
+          (ih 1 (by omega) _ finrank_euclideanSpace_fin))
 
 theorem prekopa_leindler
     {t : ℝ} (h0t : 0 < t) (ht1 : t < 1)
