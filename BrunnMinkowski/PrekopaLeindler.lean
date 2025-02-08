c33c6de4f8c2c5773e5a4539b79db38f4016d4dd
qawbecrdtey <qawbecrdtey@kaist.ac.kr>
2025-02-09 02:01:06 +0900

    TODO: Remove sorry and start main proof.

--- BrunnMinkowski/PrekopaLeindler.lean
+++ BrunnMinkowski/PrekopaLeindler.lean
@@ -15,49 +15,54 @@ open scoped Real Topology
 -- isomorhpism from any f.d. R-v.s. to R^d
 #check toEuclidean
 
-class EuclideanIsomorphismInvariant
-    (P : (α : Type) → [AddCommGroup α] → [TopologicalSpace α] → [T2Space α] → [Module ℝ α] →
-      [ContinuousSMul ℝ α] → [FiniteDimensional ℝ α] → Prop) :
+class EuclideanIsomorphismInvariant.{u}
+    (P : (α : Type u) → [AddCommGroup α] → [TopologicalSpace α] → [TopologicalAddGroup α] →
+      [T2Space α] → [Module ℝ α] → [ContinuousSMul ℝ α] → [FiniteDimensional ℝ α] → Prop) :
     Prop where
   isomorphism_invariant
-      (α : Type) [AddCommGroup α] [TopologicalSpace α] [T2Space α] [Module ℝ α]
-        [ContinuousSMul ℝ α] [FiniteDimensional ℝ α]
-      (β : Type) [AddCommGroup β] [TopologicalSpace β] [T2Space β] [Module ℝ β]
-        [ContinuousSMul ℝ β] [FiniteDimensional ℝ β]
-      (h : α ≃ₗ[ℝ] β) :
+      (α : Type u) [AddCommGroup α] [TopologicalSpace α] [TopologicalAddGroup α] [T2Space α]
+        [Module ℝ α] [ContinuousSMul ℝ α] [FiniteDimensional ℝ α]
+      (β : Type u) [AddCommGroup β] [TopologicalSpace β] [TopologicalAddGroup β] [T2Space β]
+        [Module ℝ β] [ContinuousSMul ℝ β] [FiniteDimensional ℝ β]
+      (h : α ≃L[ℝ] β) :
     P α ↔ P β
 
-example {n : ℕ} : Module.finrank ℝ (EuclideanSpace ℝ (Fin n)) = n :=
-  finrank_euclideanSpace_fin
+-- TODO: Remove sorry.
+def EuclideanSpace.finProdLinearEquiv (n₁ n₂ : ℕ) :
+    ((EuclideanSpace ℝ (Fin n₁)) × (EuclideanSpace ℝ (Fin n₂))) ≃L[ℝ]
+      EuclideanSpace ℝ (Fin (n₁ + n₂)) :=
+  ⟨sorry, sorry, sorry⟩
 
--- TODO: Fix instance issues.
 -- TODO: Consider higher order types if possible.
 theorem EuclideanSpace.induction_on_finrank
-    {P : (α : Type) → [AddCommGroup α] → [TopologicalSpace α] → [T2Space α] → [Module ℝ α] →
-      [ContinuousSMul ℝ α] → [FiniteDimensional ℝ α] → Prop} [EuclideanIsomorphismInvariant P]
-    {base0 : (α : Type) → [AddCommGroup α] → [TopologicalSpace α] → [T2Space α] → [Module ℝ α] →
-      [ContinuousSMul ℝ α] → [FiniteDimensional ℝ α] → Module.finrank ℝ α = 0 → P α}
-    {base1 : (α : Type) → [AddCommGroup α] → [TopologicalSpace α] → [T2Space α] → [Module ℝ α] →
-      [ContinuousSMul ℝ α] → [FiniteDimensional ℝ α] → Module.finrank ℝ α = 1 → P α}
-    {induct : {α : Type} → [AddCommGroup α] → [TopologicalSpace α] → [T2Space α] → [Module ℝ α] →
-      [ContinuousSMul ℝ α] → [FiniteDimensional ℝ α] →
-      {β : Type} → [AddCommGroup β] → [TopologicalSpace β] → [T2Space β] → [Module ℝ β] →
-      [ContinuousSMul ℝ β] → [FiniteDimensional ℝ β] →
+    {P : (α : Type) → [AddCommGroup α] → [TopologicalSpace α] → [TopologicalAddGroup α] →
+      [T2Space α] → [Module ℝ α] → [ContinuousSMul ℝ α] → [FiniteDimensional ℝ α] → Prop}
+    [EuclideanIsomorphismInvariant P]
+    {base0 : (α : Type) → [AddCommGroup α] → [TopologicalSpace α] → [TopologicalAddGroup α] →
+      [T2Space α] → [Module ℝ α] → [ContinuousSMul ℝ α] → [FiniteDimensional ℝ α] →
+      Module.finrank ℝ α = 0 → P α}
+    {base1 : (α : Type) → [AddCommGroup α] → [TopologicalSpace α] → [TopologicalAddGroup α] →
+      [T2Space α] → [Module ℝ α] → [ContinuousSMul ℝ α] → [FiniteDimensional ℝ α] →
+      Module.finrank ℝ α = 1 → P α}
+    {induct : {α : Type} → [AddCommGroup α] → [TopologicalSpace α] → [TopologicalAddGroup α] →
+      [T2Space α] → [Module ℝ α] → [ContinuousSMul ℝ α] → [FiniteDimensional ℝ α] →
+      {β : Type} → [AddCommGroup β] → [TopologicalSpace β] → [TopologicalAddGroup β] →
+      [T2Space β] → [Module ℝ β] → [ContinuousSMul ℝ β] → [FiniteDimensional ℝ β] →
       P α → P β → P (α × β)}
-    (α : Type) [AddCommGroup α] [TopologicalSpace α] [T2Space α] [Module ℝ α]
-    [ContinuousSMul ℝ α] [FiniteDimensional ℝ α] : P α := by
+    (α : Type) [AddCommGroup α] [TopologicalSpace α] [TopologicalAddGroup α] [T2Space α]
+    [Module ℝ α] [ContinuousSMul ℝ α] [FiniteDimensional ℝ α] : P α := by
   induction' h₀ : Module.finrank ℝ α using Nat.strong_induction_on generalizing α
-  case h n ih _ _ _ _ _ _ =>
+  case h n ih _ _ _ _ _ _ _ =>
     match n with
     | 0 => exact base0 _ h₀
     | 1 => exact base1 _ h₀
     | n + 2 =>
-      let eqv : α ≃ₗ[ℝ] (EuclideanSpace ℝ (Fin (n + 1)) × EuclideanSpace ℝ (Fin 1)) :=
-        sorry
-      exact (‹EuclideanIsomorphismInvariant P›.isomorphism_invariant α _ eqv).mpr
-        (induct
-          (ih (n + 1) (by omega) _ finrank_euclideanSpace_fin)
-          (ih 1 (by omega) _ finrank_euclideanSpace_fin))
+      let eqv : α ≃L[ℝ] (EuclideanSpace ℝ (Fin (n + 1)) × EuclideanSpace ℝ (Fin 1)) :=
+        ContinuousLinearEquiv.trans (h₀ ▸ @toEuclidean α _ _ _ _ _ _ _)
+          (EuclideanSpace.finProdLinearEquiv (n + 1) 1).symm
+      exact (‹EuclideanIsomorphismInvariant P›.isomorphism_invariant α _ eqv).mpr (induct
+        (ih (n + 1) (by omega) _ finrank_euclideanSpace_fin)
+        (ih 1 (by omega) _ finrank_euclideanSpace_fin))
 
 theorem prekopa_leindler
     {t : ℝ} (h0t : 0 < t) (ht1 : t < 1)
