6bb2480b50110ff88243cc50ea412fb643eded9d
qawbecrdtey <qawbecrdtey@kaist.ac.kr>
2025-03-12 17:57:54 +0900

    working on it.

--- BrunnMinkowski/PrekopaLeindler.lean
+++ BrunnMinkowski/PrekopaLeindler.lean
@@ -11,6 +11,13 @@ def EuclideanProd
 
 infixl:35 " ×ₑ " => EuclideanProd
 
+section
+
+variable {α : Type*} [AddCommGroup α] [TopologicalSpace α] [IsTopologicalAddGroup α] [T2Space α]
+variable [Module ℝ α] [ContinuousSMul ℝ α] [FiniteDimensional ℝ α]
+
+end
+
 section EuclideanProd
 
 variable {α : Type*} [AddCommGroup α] [TopologicalSpace α] [IsTopologicalAddGroup α] [T2Space α]
@@ -59,13 +66,31 @@ section
 
 open MeasureTheory
 
-def PrekopaLeindlerCondition
-    {t : ℝ} (ht₁ : 0 < t) (ht₂ : t < 1)
-    {ι : Type*} [Finite ι] {mι : MeasurableSpace ι} [MeasurableSingletonClass ι]
+set_option linter.unusedVariables false in
+def PrekopaLeindler.Condition
+    {t : ℝ} (ht₁ : 0 < t) (ht₂ : t < 1) {ι : Type*} [Fintype ι]
     (f : EuclideanSpace ℝ ι → ℝ) (hf₁ : Integrable f) (hf₂ : ∀ x, 0 ≤ f x)
     (g : EuclideanSpace ℝ ι → ℝ) (hg₁ : Integrable g) (hg₂ : ∀ x, 0 ≤ g x)
     (h : EuclideanSpace ℝ ι → ℝ) (hh₁ : Integrable h) : Prop :=
   ∀ x y : EuclideanSpace ℝ ι, (f x) ^ (1 - t) * (g y) ^ t ≤ h (x + y)
 
-end section
+namespace PrekopaLeindler
+
+variable {t : ℝ} (ht₁ : 0 < t) (ht₂ : t < 1)
+variable {ι : Type*} [Fintype ι]
+variable {f : EuclideanSpace ℝ ι → ℝ} (hf₁ : Integrable f) (hf₂ : ∀ x, 0 ≤ f x)
+variable {g : EuclideanSpace ℝ ι → ℝ} (hg₁ : Integrable g) (hg₂ : ∀ x, 0 ≤ g x)
+variable {h : EuclideanSpace ℝ ι → ℝ} (hh₁ : Integrable h)
+
+theorem Condition.nonnegative
+    (h₀ : Condition ht₁ ht₂ f hf₁ hf₂ g hg₁ hg₂ h hh₁) {x : EuclideanSpace ℝ ι} :
+    0 ≤ h x := by
+  have := h₀ x 0; rw [add_zero] at this; refine le_trans ?_ this
+  have := hf₂ x; have := hg₂ 0; positivity
+
+end PrekopaLeindler
+
+-- TODO: Add PrekopaLeindler.Statement
+
+end
 
