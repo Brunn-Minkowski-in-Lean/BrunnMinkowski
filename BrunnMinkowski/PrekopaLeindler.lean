ba97ac16e7811a042f78882941e9f68913d17330
qawbecrdtey <qawbecrdtey@kaist.ac.kr>
2025-03-25 01:15:18 +0900

    working on it.

--- BrunnMinkowski/PrekopaLeindler.lean
+++ BrunnMinkowski/PrekopaLeindler.lean
@@ -18,30 +18,19 @@ variable {β : Type*} [TopologicalSpace β] [MeasurableSpace β]
 --    MeasurableSpace (WithLp p (α × β)) :=
 --  m₁.comap 
 
-instance : MeasurableSpace (WithLp p α) := ‹MeasurableSpace α›
+noncomputable instance {α : Type*} [MeasurableSpace α] {p : ENNReal} [Fact (1 ≤ p)] :
+    MeasurableSpace (WithLp p α) :=
+  ‹MeasurableSpace α›
 
-instance [BorelSpace α] [BorelSpace β] [SecondCountableTopologyEither α β] :
+noncomputable instance {α β : Type*}
+    [TopologicalSpace α] [MeasurableSpace α] [BorelSpace α]
+    [TopologicalSpace β] [MeasurableSpace β] [BorelSpace β]
+    [SecondCountableTopologyEither α β] {p : ENNReal} [Fact (1 ≤ p)] :
     BorelSpace (WithLp p (α × β)) :=
-  Prod.borelSpace
+  inferInstanceAs <| BorelSpace (α × β)
 
-instance [MeasureSpace α] : MeasureSpace (WithLp p α) := ‹MeasureSpace α›
-
-instance
-    {α : Type*} [MeasureSpace α] [SFinite (volume : Measure α)]
-    {β : Type*} [MeasureSpace β] [SFinite (volume : Measure β)] :
-    SFinite (volume : Measure (WithLp p (α × β))) :=
-  haveI : SFinite (volume : Measure (α × β)) := inferInstance
-  this
-
-omit [Fact _] [TopologicalSpace _] [MeasurableSpace _] in
-theorem integral_withlp [MeasureSpace α] {f : WithLp p α → ℝ} :
-    ∫ (x : WithLp p α), f x = ∫ (x : α), f x :=
-  rfl
-
-end
-
-instance : SFinite (volume : Measure (EuclideanSpace ℝ ι)) :=
-  sorry
+--instance : SFinite (volume : Measure (EuclideanSpace ℝ ι)) :=
+--  sorry
 
 omit [Fact (1 ≤ p)] in
 theorem additional_lemma
@@ -56,7 +45,7 @@ theorem condition_of_oplus
     (hι :
       {f : EuclideanSpace ℝ ι → ℝ} → Integrable f → (∀ x, 0 ≤ f x) →
       {g : EuclideanSpace ℝ ι → ℝ} → Integrable g → (∀ y, 0 ≤ g y) →
-      {h : EuclideanSpace ℝ ι → ℝ} → Integrable h →
+    {h : EuclideanSpace ℝ ι → ℝ} → Integrable h →
       (∀ x y, (f x) ^ (1 - t) * (g y) ^ t ≤ h (x + y)) →
       (∫ x, f x) ^ (1 - t) * (∫ y, g y) ^ t ≤
       (1 - t) ^ ((Fintype.card ι) * (1 - t)) * t ^ ((Fintype.card ι) * t) * (∫ z, h z))
@@ -82,6 +71,12 @@ theorem condition_of_oplus
   let F (x₁ : EuclideanSpace ℝ ι) : EuclideanSpace ℝ κ → ℝ := fun x₂ ↦ f (eqvₗᵢ.symm (x₁, x₂))
   let G (y₁ : EuclideanSpace ℝ ι) : EuclideanSpace ℝ κ → ℝ := fun y₂ ↦ g (eqvₗᵢ.symm (y₁, y₂))
   let H (z₁ : EuclideanSpace ℝ ι) : EuclideanSpace ℝ κ → ℝ := fun z₂ ↦ h (eqvₗᵢ.symm (z₁, z₂))
+  have hF₁ {x₁} : Integrable (F x₁) := by
+    simp [F]; have := (integrable_comp eqvₗᵢ.symm f).mpr hf₁; exact?
+    
+  have hF₂ {x₁} (x₂) : 0 ≤ F x₁ x₂ := hf₂ _
+  have hG₁ {x₁} : Integrable (G x₁) := sorry
+  have hG₂ {y₁} (y₂) : 0 ≤ G y₁ y₂ := hg₂ _
   let m : MeasureTheory.MeasurePreserving eqvₗᵢ.symm.toMeasurableEquiv := sorry
   rw [← m.map_eq]
   have hf₃ := @MeasureTheory.integral_map_equiv _ _ _ _ _ volume _ _ eqvₗᵢ.symm.toMeasurableEquiv f
@@ -89,14 +84,21 @@ theorem condition_of_oplus
   have hh₃ := @MeasureTheory.integral_map_equiv _ _ _ _ _ volume _ _ eqvₗᵢ.symm.toMeasurableEquiv h
   rw [hf₃, hg₃, hh₃, LinearIsometryEquiv.coe_toMeasurableEquiv]
   have h₁ (f : WithLp 2 ((EuclideanSpace ℝ ι) × (EuclideanSpace ℝ κ)) → ℝ) :
-      ∫ (x : WithLp 2 ((EuclideanSpace ℝ ι) × (EuclideanSpace ℝ κ))), f x ∂volume.prod volume =
-      ∫ (x : (EuclideanSpace ℝ ι) × (EuclideanSpace ℝ κ)), f x ∂volume.prod volume :=
-    rfl
-  have h₂ (f : WithLp 2 ((EuclideanSpace ℝ ι) × (EuclideanSpace ℝ κ)) → ℝ) :
-      ∫ (x : WithLp 2 ((EuclideanSpace ℝ ι) × (EuclideanSpace ℝ κ))), f x ∂volume.prod volume =
-      ∫ (x : WithLp 2 ((EuclideanSpace ℝ ι) × (EuclideanSpace ℝ κ))), f x ∂volume := by
-    rw [Measure.volume_eq_prod]
-  simp_rw [← h₂, h₁]
+      ∫ (x : WithLp 2 ((EuclideanSpace ℝ ι) × (EuclideanSpace ℝ κ))), f x ∂volume =
+      ∫ (x : (EuclideanSpace ℝ ι) × (EuclideanSpace ℝ κ)), f x ∂volume.prod volume := by
+    rw [Measure.volume_eq_prod]; rfl
+  simp_rw [h₁]
+  have h₂ {x₁ y₁} :
+      (∫ x₂, F x₁ x₂) ^ (1 - t) * (∫ y₂, G y₁ y₂) ^ t ≤
+      (1 - t) ^ ((Fintype.card κ) * (1 - t)) *
+        t ^ ((Fintype.card κ) * t) * (∫ z₂, H (x₁ + y₁) z₂) := by
+    apply hκ _ hF₂ _ hG₂
+    · sorry
+    · sorry
+    · sorry
+    · sorry
+  sorry
+  /-
   rw [integral_prod, integral_prod, integral_prod] <;> (try rw [integrable_prod_iff]) <;>
   (try apply And.intro)
   · sorry
@@ -109,7 +111,7 @@ theorem condition_of_oplus
   · sorry
   · sorry
   · sorry
-
+  -/
 end PrekopaLeindler
 
 -- TODO: Add PrekopaLeindler.Statement
